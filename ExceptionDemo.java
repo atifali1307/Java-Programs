e14d87b0e4493f10f5ca7a2275501270edf6e98d
Atif Ali <94118166+atifali1307@users.noreply.github.com>
2022-05-17 10:32:13 +0530

    Add files via upload

---
+++ ExceptionDemo.java
@@ -0,0 +1,28 @@
+class ExceptionClass
+{
+  int result;
+  ExceptionClass(int num1,int num2)
+  {
+    try
+    {
+      result=num1/num2;
+    }
+    catch(ArithmeticException e)
+    {
+      System.out.println("Catch Block");
+    }
+  }
+  void getResult()
+  {
+    System.out.println("Result: "+result);
+  }
+}
+public class ExceptionDemo
+{
+  public static void main(String args[])
+  {
+    ExceptionClass ex=new ExceptionClass(25,0);
+    ex.getResult();
+    System.out.println("Exception Handled Successfully");
+  }
+}
