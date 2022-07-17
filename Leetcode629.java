cdff663e6bf661fba34dc9cd88c991070edca379
Atif Ali <94118166+atifali1307@users.noreply.github.com>
2022-07-17 12:12:56 +0530

    Add files via upload

---
+++ Leetcode629.java
@@ -0,0 +1,23 @@
+Leetcode 629 Problem
+
+K Inverse Pairs Array
+
+
+
+class Solution {
+  public int kInversePairs(int n, int k) {
+    final int kMod = (int) 1e9 + 7;
+    int[][] dp = new int[n + 1][k + 1];
+    for (int i = 0; i <= n; ++i)
+      dp[i][0] = 1;
+
+    for (int i = 1; i <= n; ++i)
+      for (int j = 1; j <= k; ++j) {
+        dp[i][j] = (dp[i][j - 1] + dp[i - 1][j]) % kMod;
+        if (j - i >= 0)
+          dp[i][j] = (dp[i][j] - dp[i - 1][j - i] + kMod) % kMod;
+      }
+
+    return dp[n][k];
+  }
+}
