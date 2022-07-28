79d190a8c4b460a269400ac7fc23545bf18fe9d0
Atif Ali <94118166+atifali1307@users.noreply.github.com>
2022-07-28 17:54:38 +0530

    Add files via upload

---
+++ Leetcode_231.java
@@ -0,0 +1,17 @@
+// Leetcode 231
+// Power of Two
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n<=0) 
+        return false;
+    while(n>2){
+        int t = n>>1;
+        int c = t<<1;
+        if(n-c != 0)
+            return false;
+        n = n>>1;
+    }
+    return true;
+    }
+}
