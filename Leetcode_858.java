c1981dd398f4dd7a1125368d15eb838b231e4908
Atif Ali <94118166+atifali1307@users.noreply.github.com>
2022-08-04 19:12:13 +0530

    Add files via upload

---
+++ Leetcode_858.java
@@ -0,0 +1,9 @@
+// Leetcode 858
+// Mirror Reflection
+
+class Solution {
+    public int mirrorReflection(int p, int q) {
+        while (p % 2 == 0 && q % 2 == 0) {p >>= 1; q >>= 1;}
+        return 1 - p % 2 + q % 2;
+    }
+}
