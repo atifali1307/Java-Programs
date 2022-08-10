43afe221f8a9c113d7a326ff3cb0b41d80e25f57
Atif Ali <94118166+atifali1307@users.noreply.github.com>
2022-08-10 20:48:41 +0530

    Add files via upload

---
+++ Leetcode_108.java
@@ -0,0 +1,20 @@
+// Leetcode 108
+// Convert Sorted Array to Binary Search Tree
+
+
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if(nums.length==0)
+            return null;
+        return sortedArrayToBST(nums,0,nums.length-1);
+    }
+    public TreeNode sortedArrayToBST(int[] num,int start,int end){
+        if(start>end)
+            return null;
+        int mid=(start+end)/2;
+        TreeNode root=new TreeNode(num[mid]);
+        root.left=sortedArrayToBST(num,start,mid-1);
+        root.right=sortedArrayToBST(num,mid+1,end);
+        return root;
+    }
+}
