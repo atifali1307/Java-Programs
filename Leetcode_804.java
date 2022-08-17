e8c641ccc379032c6eddbc0bb6062ec4169aa443
Atif Ali <94118166+atifali1307@users.noreply.github.com>
2022-08-17 21:20:07 +0530

    Add files via upload

---
+++ Leetcode_804.java
@@ -0,0 +1,16 @@
+// Leetcode 804
+// Unique Morse Code Words
+
+class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+        final String[] morse = {".-",   "-...", "-.-.", "-..",  ".",   "..-.", "--.",  "....", "..",".---", "-.-",  ".-..", "--",   "-.",  "---",  ".--.", "--.-", ".-.","...",  "-",    "..-",  "...-", ".--", "-..-", "-.--", "--.."};
+    Set<String> transformations = new HashSet<>();
+    for (final String word : words) {
+      StringBuilder transformation = new StringBuilder();
+      for (final char c : word.toCharArray())
+        transformation.append(morse[c - 'a']);
+      transformations.add(transformation.toString());
+    }
+    return transformations.size();
+    }
+}
