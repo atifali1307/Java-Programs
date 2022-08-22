0c34a63a29365ff4b618cf99407e5914663d386d
Atif Ali <94118166+atifali1307@users.noreply.github.com>
2022-08-22 20:24:51 +0530

    Add files via upload

---
+++ Leetcode_342.java
@@ -0,0 +1,12 @@
+// Leetcode 342
+// Power of Four
+
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n==0)
+            return false;
+        while(n%4==0) n/=4;
+        if(n==1) return true;
+        else return false;
+    }
+}
