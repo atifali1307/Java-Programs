7597aee7b4f8fa5a90b31d21a829e3f86525f0b4
Atif Ali <94118166+atifali1307@users.noreply.github.com>
2022-08-27 21:48:24 +0530

    Add files via upload

---
+++ Leetcode_363.java
@@ -0,0 +1,39 @@
+// Leetcode 363
+// Max Sum of Rectangle No Larger Than K
+
+class Solution {
+    public int maxSumSubmatrix(int[][] matrix, int k) {
+        if(matrix==null || matrix.length==0 || matrix[0].length==0){
+            return 0;
+        }
+        int m=matrix.length;
+        int n=matrix[0].length;
+        int result=Integer.MIN_VALUE;
+        for(int c1=0;c1<n;c1++){
+            int[] each=new int[m];
+            for(int c2=c1;c2>=0;c2--){
+                for(int r=0;r<m;r++){
+                    each[r]+=matrix[r][c2];
+                }
+                result=Math.max(result,getLargestSumCloseTok(each,k));   
+            }
+        }
+        return result;
+    }
+    public int getLargestSumCloseTok(int[] arr,int k){
+        int sum=0;
+        TreeSet<Integer> set=new TreeSet<Integer>();
+        int result=Integer.MIN_VALUE;
+        set.add(0);
+        for(int i=0;i<arr.length;i++){
+            sum=sum+arr[i];
+            Integer ceiling=set.ceiling(sum-k);
+            if(ceiling!=null){
+                result=Math.max(result,sum-ceiling);
+            }
+            set.add(sum);
+            
+        }
+        return result;
+    }
+}
