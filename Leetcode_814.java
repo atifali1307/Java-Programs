fd9318f01f5c3b2eedbde9c6b2693e345f44dcea
Atif Ali <94118166+atifali1307@users.noreply.github.com>
2022-09-06 20:32:50 +0530

    Add files via upload

---
+++ Leetcode_814.java
@@ -0,0 +1,14 @@
+// Leetcode 814
+// Binary Tree Pruning
+
+class Solution {
+    public TreeNode pruneTree(TreeNode root) {
+        if(root==null)
+            return root;
+        root.left=pruneTree(root.left);
+        root.right=pruneTree(root.right);
+        if(root.val==1 || root.left!=null || root.right!=null)
+            return root;
+        return null;
+    }
+}
